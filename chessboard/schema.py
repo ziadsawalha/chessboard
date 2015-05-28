4d566970bdd82b64d8c2a6669a2defa5302f0419
Ziad Sawalha <gerrit@highbridgellc.com>
2015-05-28 17:02:27 +0300

    feat(schema): integrate documentation in schema
    Adds a schema_docs.yaml file with all fields and
    basic type names then adds code to link those to
    the schemas defined in schema.py using an extended
    voluptuous.Schema class called DocumentedSchema.
    
    Example usage is shown in the TestVerboseParser
    test.

--- chessboard/schema.py
+++ chessboard/schema.py
@@ -14,8 +14,16 @@
 
 """Schema definition and validation utils for the Checkmatefile structure."""
 
+import logging
+import os
+import re
+
 import six
 import voluptuous as volup
+import yaml
+
+LOG = logging.getLogger(__name__)
+
 
 ###########################
 # Constants and utilities #
@@ -95,11 +103,12 @@ class DictOf(volup.Schema):
     Shamelessly copied from Checkmate and modified.
     """
 
-    def __call__(self, entry):
-        if not isinstance(entry, dict):
+    def __call__(self, data):
+        """Validate data against this schema."""
+        if not isinstance(data, dict):
             raise volup.Invalid('value not a dict')
         errors = []
-        for key, value in entry.items():
+        for key, value in data.items():
             try:
                 self.schema(value)
             except volup.MultipleInvalid as exc:
@@ -110,7 +119,7 @@ class DictOf(volup.Schema):
                 errors.extend(exc.errors)
         if errors:
             raise volup.MultipleInvalid(errors)
-        return entry
+        return data
 
 
 def Coerce(type, msg=None):
@@ -150,6 +159,227 @@ def coerce_dict(existing, changed):
     existing.update(changed)
 
 
+#
+# Documentation extensions for voluptuous schema.
+#
+DOCS_PATH = os.path.join(os.path.dirname(__file__), 'schema_docs.yaml')
+
+# This is the schema of the entries expected in the YAML documentation file.
+DOCS_SCHEMA = volup.Schema({
+    volup.Optional('type'): str,
+    'fields': object,
+    volup.Optional('description'): str,
+    volup.Optional('docs'): str,
+    volup.Optional('shorthand'): str,
+})
+
+
+def load_docs(schema_file):
+    """Load schema documentation.
+
+    Schema is expected to be a file object with multiple entries in the format
+    defined in DOCS_SCHEMA:
+        <name>: what is documented (ex. 'component')
+            type: defaults to <name> if not supplied, but can also be a
+                  'list(<name>)' or 'dict(<name>)'
+            fields:
+              <key>:
+                type: the type of the field, which can also be any <name>
+                description: short description of what the field is
+                docs: long, markdown documentation for the field
+            docs: detailed documentation in markdown
+            description: short description
+            shorthand: the formating for shorthand syntax if available
+    """
+    results = {}
+    try:
+        contents = yaml.safe_load(schema_file)
+    except IOError:
+        LOG.warning("Schema file '%s' is missing or not readable", DOCS_PATH)
+        raise
+    except ValueError as exc:
+        LOG.warning("Schema file '%s' is not valid YAML: %s", DOCS_PATH, exc)
+        raise
+
+    try:
+        for key, definition in contents.items():
+            DOCS_SCHEMA(definition)
+            definition.setdefault('type', key)
+            results[key] = definition
+    except volup.MultipleInvalid as exc:
+        LOG.warning("Schema file '%s' has bad data: %s", DOCS_PATH, exc)
+        raise
+
+    return results
+
+#
+# Initialize schema docs from schema_docs.yaml by default
+#
+DOCS = load_docs(open(DOCS_PATH, 'r'))
+
+
+def parse_type_name(type_string):
+    """Parse the string name of a type and return type and subtype.
+
+    Supports `dict(<subtype>)` and `list(<subtype>)` without recursion (i.e.
+    you cannot supply `dict(dict(<subtype>))`).
+
+    .. doctest::
+
+        >>> print parse_type_name('string')
+        ('string', None)
+        >>> print parse_type_name('list(int)')
+        ('list', 'int')
+        >>> print parse_type_name('dict(option)')
+        ('dict', 'option')
+        >>> print parse_type_name('else(option)')
+        ('else(option)', None)
+
+    :returns: tuple of two schema type names (type, subtype)
+    """
+    parsed = re.match(r'(?P<fxn>(dict|list))\((?P<type>\w*)\)', type_string)
+    if parsed:
+        return parsed.group('fxn'), parsed.group('type')
+    return type_string, None
+
+
+class DocumentedSchema(volup.Schema):
+
+    """Voluptuous Schema with documentation.
+
+    This class extends voluptuous.Schema to suppoer the following:
+
+    - store a 'name' with the schema
+    - decorate validation errors with documentation based on the name
+
+    Usage:
+
+    a) To create a voluptuous.Schema that has a name associated with it:
+
+        DocumentedSchema(Relation, name='relation')
+
+    b) To create a voluptuous.Schema that can be looked up by name:
+
+        schema = DocumentedSchema(Relation, name='relation').register()
+
+    c) To output verbose messages:
+
+        schema = DocumentedSchema(Relation, name='relation').register()
+        try:
+            schema(bad_data)
+        except voluptuous.MultipleInvalid as exc:
+            for error in exc.errors:
+                if hasattr(error, 'docs'):
+                    print error.docs
+
+    """
+
+    registered_types = {}
+
+    def __init__(self, schema, required=False, extra=volup.PREVENT_EXTRA,
+                 name=None):
+        """Extend voluptuous.Schema __init__ with `name`."""
+        self.name = name
+        super(DocumentedSchema, self).__init__(schema, required=required,
+                                               extra=extra)
+
+    def __call__(self, data):
+        """Validate data against this schema."""
+        try:
+            return super(DocumentedSchema, self).__call__(data)
+        except volup.MultipleInvalid as exc:
+            self.decorate_errors(exc.errors)
+            raise
+
+    def find_schema(self, error_path, schema=None):
+        """Find schema entry from error.path.
+
+        This function will navigate the schema based on the voluptuous error
+        path supplied. It will start from the current schema as a root (unless
+        an override is supplied in the `schema` parameter) and iterate down the
+        list of valid/allowed fields and return the last valid schema in the
+        path.
+
+        :returns: schema
+        """
+        if not schema:
+            schema = self
+
+        for node in error_path:
+            try:
+                if isinstance(schema, DocumentedSchema):
+                    schema = schema.get_field_schema(node)
+                elif isinstance(schema, DictOf):
+                    schema = schema.schema
+                elif isinstance(schema, list):
+                    schema = schema[0]
+                else:
+                    raise TypeError("Type '%s' not supported." % type(schema))
+            except KeyError:
+                return schema
+        return schema
+
+    def decorate_errors(self, errors):
+        """Add `docs` key from schema docs to each error.
+
+        This decorates an exception's `.errors` collection with docs for each
+        error if the docs exist. If no docs exist for a specific path (KeyError
+        when search in DOCS), then we won't raise a new error.
+        """
+        for error in errors:
+            if hasattr(error, 'docs'):
+                continue
+            try:
+                definition = self.find_schema(error.path)
+                if hasattr(definition, 'name'):
+                    error.docs = DOCS[definition.name]
+            except KeyError:
+                break  # No docs exist for this error's path
+
+    def register(self):
+        """Add schema to mapping with associated type name.
+
+        Allows chaining by returning self on success. For example:
+
+            label_schema = DocumentedSchema(str, name="label").register()
+
+        :raises: KeyError if registering over a pre-registered class
+        :returns: self to allow chaining
+        """
+        if (self.name in self.registered_types and
+                self is not self.registered_types[self.name]):
+            raise KeyError(self.name)
+        self.registered_types[self.name] = self
+        return self
+
+    def __repr__(self):
+        """Show `name` in repr."""
+        return '<%s %s>' % (self.__class__.__name__, self.name)
+
+    def get_field_schema(self, name):
+        """Return the schema defined for a field under this schema.
+
+        :param name: the name of a field defined under this schema.
+
+        :returns: a valid voluptuous.Schema (including types such as `dict` and
+            `list`) as defined for this field.
+
+        :raises:
+            KeyError: if docs or schema are missing or not registered.
+        """
+        field = DOCS[self.name]['fields'][name]
+        type_name, subtype_name = parse_type_name(field.get('type', 'string'))
+        if subtype_name:
+            if type_name == 'dict':
+                return DictOf(self.registered_types[subtype_name])
+            elif type_name == 'list':
+                return [self.registered_types[subtype_name]]
+            else:
+                raise ValueError("Type '%s' does not support subtypes. "
+                                 "Expected 'dict' or 'list'." % type_name)
+        return self.registered_types[type_name]
+
+
 ######################
 # Schema definitions #
 ######################
@@ -157,7 +387,7 @@ def coerce_dict(existing, changed):
 
 #: Schema for a member of
 #:  `blueprint` -> `services` -> SERVICE_NAME -> `relations`
-RELATION_SCHEMA = volup.Schema({
+RELATION_LONG_SCHEMA = volup.Schema({
     # TODO(larsbutler): This is the "long form" of relation schema.
     # We need to also define the short form.
     # Short form is just a single entry in a dict in the following format:
@@ -196,7 +426,7 @@ def Relation(msg=None, coerce=False):
 
     -   {service: interface}
     -   {service: interface#tag}
-    -   or long form (see RELATION_SCHEMA)
+    -   or long form (see RELATION_LONG_SCHEMA)
     """
     def check(entry):
         if not isinstance(entry, dict):
@@ -220,15 +450,15 @@ def Relation(msg=None, coerce=False):
                 coerce_dict(entry, changed)
             else:
                 entry = changed
-        return RELATION_SCHEMA(entry)
+        return RELATION_LONG_SCHEMA(entry)
     return check
-
+RELATION_SCHEMA = DocumentedSchema(Relation, name='relation').register()
 
 # TODO(larsbutler): This is the "long form" of constraint structure.
 # We need to define the "short form" which is:
 #   {<setting>: <value>}, where <setting> is the `setting` name and <value> is
 #   the `value`.
-CONSTRAINT_SCHEMA = volup.Schema({
+CONSTRAINT_SCHEMA = DocumentedSchema({
     volup.Required('setting'): str,
     # FIXME(larsbutler): Per a conversation with @ziadsawalha, `value` is
     # supposed to be required, but it's not. I've seen examples of "valid"
@@ -249,7 +479,7 @@ CONSTRAINT_SCHEMA = volup.Schema({
     volup.Optional('in'): list,
     volup.Optional('protocols'): list,
     volup.Optional('regex'): str,
-})
+}, name="constraint").register()
 
 # These are the values that can be supplied under `component` in a service:
 COMPONENT_SELECTOR_SCHEMA_FIELDS = volup.Schema({
@@ -279,19 +509,17 @@ COMPONENT_SELECTOR_SCHEMA = volup.All(
     RequireOne(['id', 'name', 'resource_type'])
 )
 
-
 #: Schema for a member of `blueprint` -> `services`
-SERVICE_SCHEMA = volup.Schema({
+SERVICE_SCHEMA = DocumentedSchema({
     volup.Required('component'): COMPONENT_SELECTOR_SCHEMA,
     # TODO(larsbutler): need to be more specific
-    volup.Optional('relations'): [Relation()],
+    volup.Optional('relations'): [RELATION_SCHEMA],
     volup.Optional('constraints'): [CONSTRAINT_SCHEMA],
     volup.Optional('display-name'): str,
-})
-
+}, name='service').register()
 
 #: Schema for `blueprint`
-BLUEPRINT_SCHEMA = volup.Schema({
+BLUEPRINT_SCHEMA = DocumentedSchema({
     volup.Optional('id'): str,
     volup.Optional('name'): str,
     volup.Required('services'): DictOf(SERVICE_SCHEMA),
@@ -312,14 +540,41 @@ BLUEPRINT_SCHEMA = volup.Schema({
         # master, another branch name, tag, etc.
         volup.Optional('ref'): str,
     },
-})
+}, name='blueprint').register()
 
 #: Top level Checkmatefile schema
-CHECKMATEFILE_SCHEMA = volup.Schema({
+CHECKMATEFILE_SCHEMA = DocumentedSchema({
     volup.Required('blueprint'): BLUEPRINT_SCHEMA,
     # TODO(larsbutler): Add the other sections, like `environment` and `inputs`
     volup.Optional('environment'): object,
     volup.Optional('inputs'): object,
     volup.Optional('flavors'): object,
     volup.Optional('include'): object,
-})
+}, name='checkmateFile').register()
+
+
+def generate_docs(docs=None):
+    """Return markdown page with all schema documentation.
+
+    :keyword docs: supply a documentation dict. Default reverts to `DOCS`.
+    """
+    if docs is None:
+        docs = DOCS
+    toc = []
+    body = []
+    for doc, content in docs.items():
+        if 'docs' in content:
+            # Prepare an entry with hyperlink to content
+            toc.append('[%s](#%s)' % (doc, doc))
+            # Prepare anchor with content header
+            anchor = '#### <a name="%s"></a>%s' % (doc, doc)
+            body.append('%s\n\n%s' % (anchor, content['docs']))
+    return """\
+<!--Content autogenerated from schema_docs.yaml-->
+# Glossary
+
+%s  \
+
+
+%s
+""" % ('  \n'.join(toc), '\n\n'.join(body))
